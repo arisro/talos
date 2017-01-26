369434600796d22208cdd6151d48cba04b7c363f
Aris Buzachis <buzachis.aris@gmail.com>
2017-01-26 13:44:35 +0200

    Fixed config_test

--- config/config_test.go
+++ config/config_test.go
@@ -3,7 +3,7 @@ package config
 import "testing"
 
 func TestHello(t *testing.T) {
-	if val := Hello(); val != "hello123" {
+	if val := Hello(); val != "hello" {
 		t.Fatalf("Expected 'hello', got %s instead.", val)
 	}
 }
