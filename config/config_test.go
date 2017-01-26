6bb358c4a63902a31581607ef126eb6881c6d7aa
Aris Buzachis <buzachis.aris@gmail.com>
2017-01-26 13:42:58 +0200

    Goveralls fixes.

--- config/config_test.go
+++ config/config_test.go
@@ -3,7 +3,7 @@ package config
 import "testing"
 
 func TestHello(t *testing.T) {
-	if val := Hello(); val != "hello" {
+	if val := Hello(); val != "hello123" {
 		t.Fatalf("Expected 'hello', got %s instead.", val)
 	}
 }
